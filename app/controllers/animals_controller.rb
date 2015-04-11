10af4f4828544a06606e95ff1a2b078af6bb4175
Josef Zacek <info@josefzacek.com>
2015-04-11 22:51:04 +0100

    some logic added to model and showed in view

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,4 +1,7 @@
 class AnimalsController < ApplicationController
   def index
+    @name = 'Josef'
+    @surname = 'Zacek'
+    @animal_description = Animal.animal_description
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404212642) do
+ActiveRecord::Schema.define(version: 20150411213246) do
+
+  create_table "animals", force: :cascade do |t|
+    t.string   "name"
+    t.string   "farm_name"
+    t.integer  "weight"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float    "price"
+  end
 
   create_table "commentaries", force: :cascade do |t|
     t.string   "title"
