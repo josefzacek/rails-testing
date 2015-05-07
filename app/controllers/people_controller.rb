3b560a67e585408e7110d630502578106561625c
Josef Zacek <info@josefzacek.com>
2015-05-07 22:32:14 +0100

    declaring variable in controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,5 +1,8 @@
 class PeopleController < ApplicationController
   def index
     @people = Person.all
+    @hash = { name: 'josef', surname: 'zacek' }
+    @array = [1, 2, 3, 4, 5]
+    @hash_inside_array = [{ name: 'josef', surname: 'zacek' }, { name: 'mike', surname: 'smyth' }]
   end
 end
